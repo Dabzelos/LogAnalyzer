bd4cf6e20cb6f19fbe17e4fb4ec0e5c46bbe6ee0
Dabzelos <dabzelos@gmail.com>
2024-11-05 18:17:37 +0300

    feat: Parser func for parcing single log

--- internal/domain/analysis.go
+++ internal/domain/analysis.go
@@ -1,5 +1,12 @@
 package domain
 
+import (
+	"bufio"
+	"io"
+	"regexp"
+	"strconv"
+)
+
 type StatHolder struct {
 	totalCounter          int
 	httpRequests          map[string]int
@@ -18,4 +25,28 @@ func NewStatHolder() *StatHolder {
 		requestedResources: make(map[string]int),
 		commonAnswers:      make(map[string]int, 63), // вроде как существует 63 стандартных кода ответа
 	}
+}
+
+func (s *StatHolder) DataProcessor(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		singleLog := scanner.Text()
+		s.Parser(singleLog)
+	}
+}
+
+func (s *StatHolder) Parser(singleLog string) {
+	logsFormat := regexp.MustCompile("^(\\S+) - (\\S*) \\[(.*?)] \"(\\S+) (\\S+) (\\S+)\" (\\d{3}) (\\d+) \"(.*?)\" \"(.*?)\"$")
+	matches := logsFormat.FindStringSubmatch(singleLog)
+
+	if matches != nil {
+		s.totalCounter++
+		s.httpRequests[matches[4]]++
+		s.requestedResources[matches[5]]++
+		bytesInSingleLog, _ := strconv.Atoi(matches[8])
+		s.bytesSend = append(s.bytesSend, bytesInSingleLog)
+		s.commonAnswers[matches[7]]++
+		return
+	}
+	s.unparsedLogs++
 }
