c8df4bdd4647b69ea08162897d3ccd0b616788a8
Dabzelos <dabzelos@gmail.com>
2024-11-09 00:51:40 +0300

    feat: application struct + setUp methods

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,18 +1,48 @@
 package application
 
-import "time"
+import (
+	"backend_academy_2024_project_3-go-Dabzelos/internal/domain"
+	"backend_academy_2024_project_3-go-Dabzelos/internal/domain/reporters"
+	"bufio"
+	"io"
+	"time"
+)
 
 type Reporter interface {
-	ReportBuilder()
+	ReportBuilder(s *domain.Statistic)
 }
 
 type Application struct {
-	pathOrUrl string
-	reporter  Reporter
-	timeFrom  time.Time
-	timeTo    time.Time
+	pathOrURL  []string
+	Reporter   Reporter
+	RawData    *domain.DataHolder
+	Statistics *domain.Statistic
 }
 
-func (a *Application) Start() {}
+func (a *Application) Start() {
 
-func (a *Application) Parse() {}
+	// тут логика прочтения относительно взятия файла // http запроса пока это остается доделать
+}
+
+func (a *Application) SetUp() {
+	from := ""
+	to := ""
+	timeFrom, _ := time.Parse("02/Jan/2006:15:04:05 -0700", from)
+
+	timeTo, _ := time.Parse("02/Jan/2006:15:04:05 -0700", to)
+
+	a.RawData = domain.NewDataHolder(timeFrom, timeTo)
+	a.Statistics = &domain.Statistic{}
+	a.Reporter = &reporters.ReportMd{}
+}
+
+func (a *Application) DataProcessor(r io.Reader) {
+	//for _, source := range r {
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		singleLog := scanner.Text()
+		a.RawData.Parser(singleLog)
+	}
+	//	}
+}
