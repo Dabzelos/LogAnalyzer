293ba9adc89b55557e4b1fccfbeac8a51445d4eb
Dabzelos <dabzelos@gmail.com>
2024-11-09 01:48:44 +0300

    feat: more statistics in md builderToMd.go

--- internal/domain/reporters/builderToMd.go
+++ internal/domain/reporters/builderToMd.go
@@ -35,8 +35,11 @@ func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 	markdown += fmt.Sprintf("|    Начальная дата     |   %s |\n", stat.TimeRange.From.Format("02.01.2006"))
 	markdown += fmt.Sprintf("|     Конечная дата     |   %s |\n", stat.TimeRange.To.Format("02.01.2006"))
 	markdown += fmt.Sprintf("|  Количество запросов  |       %d |\n", stat.LogsMetrics.ProcessedLogs)
-	markdown += fmt.Sprintf("| Средний размер ответа |   %.2fb |\n", stat.LogsMetrics.AverageAnswerSize)
-	markdown += fmt.Sprintf("|   95p размера ответа  |   %.2fb |\n\n", stat.Percentile)
+	markdown += fmt.Sprintf("| Средний размер ответа |   %.2f |\n", stat.LogsMetrics.AverageAnswerSize)
+	markdown += fmt.Sprintf("|   95p размера ответа  |   %.2f |\n", stat.NinetyFivePercentile)
+	markdown += fmt.Sprintf("|Медиана размера ответа |   %.2f |\n", stat.Median)
+	markdown += fmt.Sprintf("|  Всего кодов ошибок   |	%d|\n", stat.ResponseCodes.ServerError+stat.ResponseCodes.ClientError)
+	markdown += fmt.Sprintf("| Процент кодов ошибок от общего числа| %.2f |\n", stat.ErrorRate)
 
 	markdown += "#### Топ HTTP запросов\n\n"
 	markdown += fmt.Sprintf("|   Запрос   | Количество |\n|:----------:|-----------:|\n")
@@ -50,7 +53,15 @@ func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", res.Value, res.Count)
 	}
 
-	markdown += "\n#### Топ HTTP кодов ответа\n\n"
+	markdown += "#### Коды ответа\n\n"
+	markdown += "| Категория      | Количество |\n|:--------------:|-----------:|\n"
+	markdown += fmt.Sprintf("| Информационные | %d         |\n", stat.ResponseCodes.Informational)
+	markdown += fmt.Sprintf("| Успешные       | %d         |\n", stat.ResponseCodes.Success)
+	markdown += fmt.Sprintf("| Перенаправления| %d         |\n", stat.ResponseCodes.Redirection)
+	markdown += fmt.Sprintf("| Ошибки клиента | %d         |\n", stat.ResponseCodes.ClientError)
+	markdown += fmt.Sprintf("| Ошибки сервера | %d         |\n\n", stat.ResponseCodes.ServerError)
+
+	markdown += "#### Топ HTTP кодов ответа\n\n"
 	markdown += fmt.Sprintf("| Код ответа | Количество |\n|:----------:|-----------:|\n")
 	for _, code := range stat.CommonStats.HTTPCode {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", code.Value, code.Count)
