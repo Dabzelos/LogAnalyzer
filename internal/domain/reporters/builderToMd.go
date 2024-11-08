bce24ba2fc4d1ecf0f65e6ab1e8876bb6d5be678
Dabzelos <dabzelos@gmail.com>
2024-11-09 01:53:20 +0300

    fix: deleted some sprintf abuse

--- internal/domain/reporters/builderToMd.go
+++ internal/domain/reporters/builderToMd.go
@@ -30,7 +30,7 @@ func (r *ReportMd) ReportBuilder(s *domain.Statistic) {
 
 func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 	markdown := "#### Общая информация\n\n"
-	markdown += fmt.Sprintf("|        Метрика        |     Значение |\n|:---------------------:|-------------:|\n")
+	markdown += "|        Метрика        |     Значение |\n|:---------------------:|-------------:|\n"
 	markdown += fmt.Sprintf("|       Файл(-ы)        | `%s` |\n", "")
 	markdown += fmt.Sprintf("|    Начальная дата     |   %s |\n", stat.TimeRange.From.Format("02.01.2006"))
 	markdown += fmt.Sprintf("|     Конечная дата     |   %s |\n", stat.TimeRange.To.Format("02.01.2006"))
@@ -41,19 +41,19 @@ func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 	markdown += fmt.Sprintf("|  Всего кодов ошибок   |	%d|\n", stat.ResponseCodes.ServerError+stat.ResponseCodes.ClientError)
 	markdown += fmt.Sprintf("| Процент кодов ошибок от общего числа| %.2f |\n", stat.ErrorRate)
 
-	markdown += "#### Топ HTTP запросов\n\n"
-	markdown += fmt.Sprintf("|   Запрос   | Количество |\n|:----------:|-----------:|\n")
+	markdown += "\n#### Топ HTTP запросов\n\n"
+	markdown += "|   Запрос   | Количество |\n|:----------:|-----------:|\n"
 	for _, req := range stat.CommonStats.HTTPRequest {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", req.Value, req.Count)
 	}
 
 	markdown += "\n#### Топ запрашиваемых ресурсов\n\n"
-	markdown += fmt.Sprintf("|   Ресурс   | Количество |\n|:----------:|-----------:|\n")
+	markdown += "|   Ресурс   | Количество |\n|:----------:|-----------:|\n"
 	for _, res := range stat.CommonStats.Resource {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", res.Value, res.Count)
 	}
 
-	markdown += "#### Коды ответа\n\n"
+	markdown += "\n#### Коды ответа\n\n"
 	markdown += "| Категория      | Количество |\n|:--------------:|-----------:|\n"
 	markdown += fmt.Sprintf("| Информационные | %d         |\n", stat.ResponseCodes.Informational)
 	markdown += fmt.Sprintf("| Успешные       | %d         |\n", stat.ResponseCodes.Success)
@@ -62,7 +62,7 @@ func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 	markdown += fmt.Sprintf("| Ошибки сервера | %d         |\n\n", stat.ResponseCodes.ServerError)
 
 	markdown += "#### Топ HTTP кодов ответа\n\n"
-	markdown += fmt.Sprintf("| Код ответа | Количество |\n|:----------:|-----------:|\n")
+	markdown += "| Код ответа | Количество |\n|:----------:|-----------:|\n"
 	for _, code := range stat.CommonStats.HTTPCode {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", code.Value, code.Count)
 	}
