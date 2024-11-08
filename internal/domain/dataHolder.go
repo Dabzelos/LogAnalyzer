48a15e3b7dcd3d00e11b9c35ae9434f92438d967
Dabzelos <dabzelos@gmail.com>
2024-11-09 00:51:01 +0300

    feat: new Data holder +commentary and Parser method + commentary

--- internal/domain/dataHolder.go
+++ internal/domain/dataHolder.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -22,4 +24,48 @@ type DataHolder struct {
 	commonAnswers      map[string]int
 	from               time.Time
 	to                 time.Time
+}
+
+// NewDataHolder - принимает параметрами timeFrom и timeTo, и инициализирует map`ы которые потом пригодятся для анализа.
+// На go.dev написано, что "нулевое значение", для time.Time это January 1, year 1, 00:00:00 UTC.
+// Это удобно тк в мы сможем воспользоваться в методе Parser при проверке заданы ли вообще временные рамки для логов.
+func NewDataHolder(timeFrom, timeTo time.Time) *DataHolder {
+	return &DataHolder{
+		httpRequests:       make(map[string]int, 9),  // в http 1.1 определенно 9 стандартных методов, р
+		requestedResources: make(map[string]int),     // решил указать тк на лекциях сказали что в рантайме может сказаться на производительности
+		commonAnswers:      make(map[string]int, 63), // вроде как существует 63 стандартных кода ответа
+		to:                 timeTo,
+		from:               timeFrom,
+	}
+}
+
+// Parser метод структуры DataHolder, принимает строку singleLog в качестве аргумента, и пытается с помощью регулярного
+// выражения, разбить на подстроки уже пригодные для анализа.
+func (s *DataHolder) Parser(singleLog string) {
+	logsFormat := regexp.MustCompile("^(\\S+) - (\\S*) \\[(.*?)] \"(\\S+) (\\S+) (\\S+)\" (\\d{3}) (\\d+) \"(.*?)\" \"(.*?)\"$")
+	matches := logsFormat.FindStringSubmatch(singleLog)
+
+	logTime, err := time.Parse("02/Jan/2006:15:04:05 -0700", matches[3])
+	if err != nil {
+		s.unparsedLogs++
+
+		return
+	}
+	// Проверка попадает ли лог в выбранный временной промежуток если он задан
+	if (!s.from.IsZero() && logTime.Before(s.from)) || (!s.to.IsZero() && logTime.After(s.to)) {
+		return
+	}
+	// после того как я проверил что лог во временном промежутке, собираем то что смогли спарсить, если смогли
+	// в противном случае увеличиваем число неспаршенных логов
+	if matches != nil {
+		s.TotalCounter++
+		s.httpRequests[matches[4]]++
+		s.requestedResources[matches[5]]++
+		bytesInSingleLog, _ := strconv.Atoi(matches[8])
+		s.bytesSend = append(s.bytesSend, bytesInSingleLog)
+		s.commonAnswers[matches[7]]++
+
+		return
+	}
+	s.unparsedLogs++
 }
