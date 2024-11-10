67ab3ab5fff30ad3e61ea2f0f5467ad2a24f0dc8
Dabzelos <dabzelos@gmail.com>
2024-11-10 22:44:00 +0300

    fix linter fixes

--- internal/domain/reporters/builderToAdoc.go
+++ internal/domain/reporters/builderToAdoc.go
@@ -21,60 +21,71 @@ func (r *ReportADoc) ReportBuilder(s *domain.Statistic) {
 
 	reportMessage := r.buildReportMessageAdoc(s)
 	_, err = file.WriteString(reportMessage)
+
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func (r *ReportADoc) buildReportMessageAdoc(stat *domain.Statistic) string {
+	const (
+		header    = "[options=\"header\"]\n|=================\n"
+		headerEnd = "|=================\n\n"
+	)
+
 	adoc := "= Log Analyzer Report\n\n"
 	adoc += "== Общая информация\n\n"
-	adoc += "[options=\"header\"]\n|=============================\n"
+	adoc += header
 	adoc += "| Метрика | Значение\n"
-	adoc += "| Файл(-ы) | " + "неизвестный файл" + "\n" // Замените значение, если нужно
+	adoc += "| Файл(-ы) | " + "" + "\n" // Замените значение, если нужно
 	adoc += fmt.Sprintf("| Начальная дата | %s\n", stat.TimeRange.From.Format("02.01.2006"))
 	adoc += fmt.Sprintf("| Конечная дата | %s\n", stat.TimeRange.To.Format("02.01.2006"))
 	adoc += fmt.Sprintf("| Количество запросов | %d\n", stat.LogsMetrics.ProcessedLogs)
 	adoc += fmt.Sprintf("| Средний размер ответа | %.2f\n", stat.LogsMetrics.AverageAnswerSize)
+	adoc += fmt.Sprintf("|Нераспаршенных логов | %d", stat.LogsMetrics.UnparsedLogs)
 	adoc += fmt.Sprintf("| 95-й перцентиль размера ответа | %.2f\n", stat.NinetyFivePercentile)
 	adoc += fmt.Sprintf("| Медиана размера ответа | %.2f\n", stat.Median)
 	adoc += fmt.Sprintf("| Всего кодов ошибок | %d\n", stat.ResponseCodes.ServerError+stat.ResponseCodes.ClientError)
 	adoc += fmt.Sprintf("| Процент кодов ошибок от общего числа | %.2f\n", stat.ErrorRate)
-	adoc += "|=============================\n\n"
+	adoc += headerEnd
 
 	adoc += "== Топ HTTP запросов\n\n"
-	adoc += "[options=\"header\"]\n|=================\n"
+	adoc += header
 	adoc += "| Запрос | Количество\n"
+
 	for _, req := range stat.CommonStats.HTTPRequest {
 		adoc += fmt.Sprintf("| %s | %d\n", req.Value, req.Count)
 	}
-	adoc += "|=================\n\n"
 
+	adoc += headerEnd
 	adoc += "== Топ запрашиваемых ресурсов\n\n"
-	adoc += "[options=\"header\"]\n|=================\n"
+	adoc += header
 	adoc += "| Ресурс | Количество\n"
+
 	for _, res := range stat.CommonStats.Resource {
 		adoc += fmt.Sprintf("| %s | %d\n", res.Value, res.Count)
 	}
-	adoc += "|=================\n\n"
 
+	adoc += headerEnd
 	adoc += "== Коды ответа\n\n"
-	adoc += "[options=\"header\"]\n|=================\n"
+	adoc += header
 	adoc += "| Категория | Количество\n"
 	adoc += fmt.Sprintf("| Информационные | %d\n", stat.ResponseCodes.Informational)
 	adoc += fmt.Sprintf("| Успешные | %d\n", stat.ResponseCodes.Success)
 	adoc += fmt.Sprintf("| Перенаправления | %d\n", stat.ResponseCodes.Redirection)
 	adoc += fmt.Sprintf("| Ошибки клиента | %d\n", stat.ResponseCodes.ClientError)
 	adoc += fmt.Sprintf("| Ошибки сервера | %d\n", stat.ResponseCodes.ServerError)
-	adoc += "|=================\n\n"
+	adoc += headerEnd
 
 	adoc += "== Топ HTTP кодов ответа\n\n"
-	adoc += "[options=\"header\"]\n|=================\n"
+	adoc += header
 	adoc += "| Код ответа | Количество\n"
+
 	for _, code := range stat.CommonStats.HTTPCode {
 		adoc += fmt.Sprintf("| %s | %d\n", code.Value, code.Count)
 	}
-	adoc += "|=================\n\n"
+
+	adoc += headerEnd
 
 	return adoc
 }
--- internal/domain/reporters/builderToMd.go
+++ internal/domain/reporters/builderToMd.go
@@ -22,6 +22,7 @@ func (r *ReportMd) ReportBuilder(s *domain.Statistic) {
 	}(file)
 
 	reportMessage := r.buildReportMessage(s)
+
 	_, err = file.WriteString(reportMessage)
 	if err != nil {
 		fmt.Println(err)
@@ -32,23 +33,26 @@ func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 	markdown := "#### Общая информация\n\n"
 	markdown += "|        Метрика        |     Значение |\n|:---------------------:|-------------:|\n"
 	markdown += fmt.Sprintf("|       Файл(-ы)        | `%s` |\n", "")
-	markdown += fmt.Sprintf("|    Начальная дата     |   %s |\n", stat.TimeRange.From.Format("02.01.2006"))
-	markdown += fmt.Sprintf("|     Конечная дата     |   %s |\n", stat.TimeRange.To.Format("02.01.2006"))
-	markdown += fmt.Sprintf("|  Количество запросов  |       %d |\n", stat.LogsMetrics.ProcessedLogs)
-	markdown += fmt.Sprintf("| Средний размер ответа |   %.2f |\n", stat.LogsMetrics.AverageAnswerSize)
-	markdown += fmt.Sprintf("|   95p размера ответа  |   %.2f |\n", stat.NinetyFivePercentile)
-	markdown += fmt.Sprintf("|Медиана размера ответа |   %.2f |\n", stat.Median)
-	markdown += fmt.Sprintf("|  Всего кодов ошибок   |	%d|\n", stat.ResponseCodes.ServerError+stat.ResponseCodes.ClientError)
+	markdown += fmt.Sprintf("|    Начальная дата     |  %s  |\n", stat.TimeRange.From.Format("02.01.2006"))
+	markdown += fmt.Sprintf("|     Конечная дата     |  %s  |\n", stat.TimeRange.To.Format("02.01.2006"))
+	markdown += fmt.Sprintf("|  Количество запросов  |  %d  |\n", stat.LogsMetrics.ProcessedLogs)
+	markdown += fmt.Sprintf("| Средний размер ответа | %.2f |\n", stat.LogsMetrics.AverageAnswerSize)
+	markdown += fmt.Sprintf("| Нераспаршенных логов  |  %d  | \n", stat.LogsMetrics.UnparsedLogs)
+	markdown += fmt.Sprintf("|   95p размера ответа  | %.2f |\n", stat.NinetyFivePercentile)
+	markdown += fmt.Sprintf("|Медиана размера ответа | %.2f |\n", stat.Median)
+	markdown += fmt.Sprintf("|  Всего кодов ошибок   |  %d  |\n", stat.ResponseCodes.ServerError+stat.ResponseCodes.ClientError)
 	markdown += fmt.Sprintf("| Процент кодов ошибок от общего числа| %.2f |\n", stat.ErrorRate)
 
 	markdown += "\n#### Топ HTTP запросов\n\n"
 	markdown += "|   Запрос   | Количество |\n|:----------:|-----------:|\n"
+
 	for _, req := range stat.CommonStats.HTTPRequest {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", req.Value, req.Count)
 	}
 
 	markdown += "\n#### Топ запрашиваемых ресурсов\n\n"
 	markdown += "|   Ресурс   | Количество |\n|:----------:|-----------:|\n"
+
 	for _, res := range stat.CommonStats.Resource {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", res.Value, res.Count)
 	}
@@ -63,6 +67,7 @@ func (r *ReportMd) buildReportMessage(stat *domain.Statistic) string {
 
 	markdown += "#### Топ HTTP кодов ответа\n\n"
 	markdown += "| Код ответа | Количество |\n|:----------:|-----------:|\n"
+
 	for _, code := range stat.CommonStats.HTTPCode {
 		markdown += fmt.Sprintf("| %-10s | %10d |\n", code.Value, code.Count)
 	}
