496abc2acb29c7838d15ab0245dce1903e533f72
Dabzelos <dabzelos@gmail.com>
2024-11-10 22:44:22 +0300

    feat: custom errors

---
+++ internal/domain/reporters/errors/errors.go
@@ -0,0 +1,47 @@
+package errors
+
+type ErrOpenFile struct{}
+
+func (e ErrOpenFile) Error() string {
+	return "open file error"
+}
+
+type ErrCloseFile struct{}
+
+func (e ErrCloseFile) Error() string {
+	return "close file error"
+}
+
+type ErrOpenURL struct{}
+
+func (e ErrOpenURL) Error() string {
+	return "open URL error"
+}
+
+type ErrCloseURL struct{}
+
+func (e ErrCloseURL) Error() string {
+	return "close URL error"
+}
+
+type ErrNoSource struct{}
+
+func (e ErrNoSource) Error() string {
+	return "no source error"
+}
+
+type ErrZeroTime struct{}
+
+func (e ErrZeroTime) Error() string {
+	return "zero time error"
+}
+
+type ErrTimeParsing struct{}
+
+func (e ErrTimeParsing) Error() string {
+	return "err parsing time"
+}
+
+type ErrWrongTimeBoundaries struct{}
+
+func (e ErrWrongTimeBoundaries) Error() string { return "to before from" }
