1fab0a1c06500fe44a74d9c4ec7800a6e222e748
Dabzelos <dabzelos@gmail.com>
2024-11-11 06:04:46 +0300

    feat: main with logger

--- cmd/LogAnalyzer/main.go
+++ cmd/LogAnalyzer/main.go
@@ -1,24 +1,15 @@
 package main
 
-import "backend_academy_2024_project_3-go-Dabzelos/internal/application"
+import (
+	"backend_academy_2024_project_3-go-Dabzelos/internal/application"
+	"backend_academy_2024_project_3-go-Dabzelos/pkg/logger"
+)
 
 func main() {
-	application.Start()
+	fileLogger := logger.NewFileLogger("logs.txt")
+
+	defer fileLogger.Close()
+	app := application.NewApp(fileLogger.Logger())
+
+	app.Start()
 }
-
-/*
-	from := "2006-01-02T15:04:05Z"
-	to := ""
-
-	_, err := time.Parse(time.RFC3339, from)
-
-	if err != nil {	fmt.Println(err)
-	}
-
-	_, err = time.Parse(time.RFC3339, to)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	//fmt.Printf("timeFrom: %v\n", timeFrom)
-*/
